dccf0ac3b200efa657ef12b8ad7a8bfb442f2cec
Kaiaba <144903113+Kaiaba@users.noreply.github.com>
2023-09-13 12:56:35 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Ciao mondo magico! \n gia'");
+		System.out.println("per ben due volte, eh si' ");
+
+	}
+
+}
